17227f7df8d1c5cc16a4ece1fe760ce44be20ca1
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-04-02 09:46:56 +0000

    Cancel network requests

--- lib/providers/activity_provider.dart
+++ lib/providers/activity_provider.dart
@@ -4,8 +4,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
 final activityProvider = FutureProvider.autoDispose<Activity>((ref) async {
+  // We create an HTTP client using package:http
+  final client = http.Client();
+  // On dispose, we close the client.
+  // This will cancel any pending request that the client might have.
+  ref.onDispose(client.close);
+
+  // We now use the client to make the request instead of the "get" function.
   final url = Uri.https('boredapi.com', '/api/activity');
-  final response = await http.get(url);
+  final response = await client.get(url);
+
   final json = jsonDecode(response.body) as Map<String, dynamic>;
   return Activity.fromJson(json);
 });

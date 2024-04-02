68704776868ab1c2393793edea6c31fa8f1668c5
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-04-02 09:58:05 +0000

    Add an extension for debouncing and cancelling network requests

--- lib/providers/activity_provider.dart
+++ lib/providers/activity_provider.dart
@@ -1,30 +1,15 @@
 import 'dart:convert';
+import 'package:bored_app/extensions/debounce_and_cancel_extension.dart';
 import 'package:bored_app/models/activity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
 
 final activityProvider = FutureProvider.autoDispose<Activity>((ref) async {
-  // We capture whether the provider is currently disposed or not.
-  var didDispose = false;
-  ref.onDispose(() => didDispose = true);
-
-  // We delay the request by 500ms, to wait for the user to stop refreshing.
-  await Future<void>.delayed(const Duration(milliseconds: 500));
-
-  // If the provider was disposed during the delay, it means that the user
-  // refreshed again. We throw an exception to cancel the request.
-  // It is safe to use an exception here, as it will be caught by Riverpod.
-  if (didDispose) {
-    throw Exception('Cancelled');
-  }
-
-  // We create an HTTP client using package:http
-  final client = http.Client();
-  // On dispose, we close the client.
-  // This will cancel any pending request that the client might have.
-  ref.onDispose(client.close);
+  // We obtain an HTTP client using the extension we created earlier.
+  final client = await ref.getDebouncedHttpClient();
 
   // We now use the client to make the request instead of the "get" function.
+  // Our request will naturally be debounced and be cancelled if the user
+  // leaves the page.
   final url = Uri.https('boredapi.com', '/api/activity');
   final response = await client.get(url);
 
